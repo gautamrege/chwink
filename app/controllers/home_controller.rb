f555d19c355d4740fbaac2aa14980ea4129cc0fd
Sethupathi Asokan <sethu@joshsoftware.com>
2012-10-19 17:46:41 +0530

    Added button in home carousel to navigate
    to chwink detail page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @chwinks = Chwink.all
+    @chwinks = [Chwink.new(:name => 'Test', :end_year => 2001, :description => 'Description comes here as well goes there and come here and soon to test the very long description which can move the like button very low so that i will get hidden')]#Chwink.all
   end
 end
