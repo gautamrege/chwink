8887369dc7d29c308397e04934c731ee843e4559
shifa <shifarules@gmail.com>
2013-07-09 10:42:22 +0530

    delete duplicate

--- spec/controllers/chwinks_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe ChwinksController do
-  context "Sharing links" do
-    it "should redirect to facebook popup when clicked on link"
-    it "should redirect to twitter popup when linked on link"
-    it "should redirect to chwink's id page when clicked on I would like to vote"
-  end
-end
