dfb19606b8ecc6b79459508293f1a2fc3e98de53
shifa <shifarules@gmail.com>
2013-07-26 15:46:35 +0530

    autocomplete in 'add a chwink' name field

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,45 +1,9 @@
-/*  $("#search").typeahead({                                
-
-    source: [ "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" ],
-    items: 3                                                                   
-    //var text = $('#search').val(); 
-    
-    var search_data ={
-     
-        "query": {"query_string": {"query": "name.autocomplete:" + $('#search').val()}},
-        "size": 3
-     
-    };
-    
-    source: function(query,process){
-      return $.ajax({
-        url: 'http://localhost:9200/chwink_development/chwink/_search',
-        type: 'GET',
-        crossDomain: true,
-        //contentType: 'application/json; charset=UTF-8',
-        //success: function(data) { console.log(data) }
-        dataType: 'json',
-        data: JSON.stringify(search_data),
-        success: function(data){
-          alert(data);
-        }
-      });
-      //var jsonData = jQuery.parseJSON(json.responseText);
-    }
-    
-
-      
-  });
-*/
-
-
 $(document).ready(function() {
-  $( "#trial" ).autocomplete({
+  $( "#query, #new_chwink" ).autocomplete({
     source: function( request, response ) {
       var postData = {
         "query": {"query_string" : { "q": "name.autocomplete:"+request.term }}
       };
-      console.log(JSON.stringify(postData));
       $.ajax({
         url: "http://localhost:9200/chwink_development/chwink/_search?q=name.autocomplete:"+request.term,
         //url: "http://localhost:9200/chwink_development/chwink/_search?",
